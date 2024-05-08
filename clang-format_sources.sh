e79619993e926e5994139cf6fe3bcab2163ead5f
Stephen Seo <seo.disparate@gmail.com>
2024-05-08 15:06:06 +0900

    Fix clang-format_sources.sh script

--- clang-format_sources.sh
+++ clang-format_sources.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 
-find "$(pwd)/src" -regex '.*\.\(h\|cc\)$' -exec echo 'Formatting {}...' ';' -execdir clang-format -i --style=Google '{}' ';'
+pushd "$(dirname "$0")" >&/dev/null
+PATH_TO_SCRIPT="$(pwd)"
+popd >&/dev/null
+
+find "${PATH_TO_SCRIPT}/src" -regex '.*\.\(h\|cc\)$' -exec echo 'Formatting {}...' ';' -execdir clang-format -i --style=Google '{}' ';'
